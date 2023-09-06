20b5276ff67ee887c206602bd46ccaf293a36cd7
Vitormatos07 <vitormatos9870@gmail.com>
2023-09-06 12:31:44 -0300

    Concluído

---
+++ Vetores/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Course
+{
+class Student
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Student(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+    public override string ToString()
+    {
+        return Name + ", " + Email;
+    }
+}
+}
